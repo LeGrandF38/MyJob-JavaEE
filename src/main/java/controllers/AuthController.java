d82ee110893baac189686e87d7fddf4d4c2123d9
LeGrandF38 <faycalyacoubaprog@gmail.com>
2024-02-26 18:26:27 +0100

    add AuthControllerServlet

---
+++ src/main/java/controllers/AuthController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.io.*;
+
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//
+//@WebServlet(name = "AuthControllerServlet", value = "/AuthController-servlet")
+@WebServlet("/AuthControllerServlet")
+public class AuthController extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/views/login.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 26/02/2024
+  Time: 17:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login</title>
+
+</head>
+<body>
+    <h1>Login page</h1>
+
+</body>
+</html>
